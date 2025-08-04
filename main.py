a1b5ef7a8a03090d33d0d111b2e72e18b3dc9b6a
yatagarasu-lab <egasira.0909@icloud.com>
2025-08-04 20:23:11 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -7,4 +8,6 @@ def index():
     return "E.TCODE is running!", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    # Render環境ではPORTが自動で設定されるので、それを取得
+    port = int(os.environ.get("PORT", 5000))  # ローカル実行時は5000番
+    app.run(host="0.0.0.0", port=port)
